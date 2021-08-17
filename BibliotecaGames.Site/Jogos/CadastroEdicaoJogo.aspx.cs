a0526967e15d9244eea7c764887eacfa4c55b3fe
lumilitas <lucasmengineer@edu.unifor.br>
2021-08-17 09:10:21 -0300

    Implementada o listagem dos generos e editores no combo

--- BibliotecaGames.Site/Jogos/CadastroEdicaoJogo.aspx.cs
+++ BibliotecaGames.Site/Jogos/CadastroEdicaoJogo.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BibliotecaGames.BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,14 +10,38 @@ namespace BibliotecaGames.Site.Jogos
 {
 	public partial class CadastroEdicaoJogo : System.Web.UI.Page
 	{
+		private GeneroBo _generoBo;
+		private EditorBo _editorBo;
 		protected void Page_Load(object sender, EventArgs e)
 		{
-
+			if (!Page.IsPostBack)
+			{
+				CarregarGenerosNaCombo();
+				CarregarEditoresNaCombo();
+			}
 		}
 
 		protected void BtnGravar_Click(object sender, EventArgs e)
 		{
 
+		}
+
+		private void CarregarEditoresNaCombo()
+		{
+			_editorBo = new EditorBo();
+			var editores = _editorBo.ObterTodosOsEditores();
+
+			DdlEditor.DataSource = editores;
+			DdlEditor.DataBind();
+		}
+
+		private void CarregarGenerosNaCombo()
+		{
+			_generoBo = new GeneroBo();
+			var generos = _generoBo.ObterTodosOsGeneros();
+
+			DdlGenero.DataSource = generos;
+			DdlGenero.DataBind();
 		}
 	}
 }
